760671d9662389035fcb9a1c3a3c72dc4d618a3a
GiYeong Um <66294595+GiYeongUM@users.noreply.github.com>
2022-10-05 00:43:16 +0900

    feat: bnb, side bar 반응형 제작

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:giyeong_um_porfolio_page/controller/home_page_controller.dart';
 import 'package:giyeong_um_porfolio_page/controller/responsive_controller.dart';
+import 'package:giyeong_um_porfolio_page/theme_data.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({Key? key}) : super(key: key);
@@ -12,9 +13,11 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-        children: <Widget>[
-          Obx(() => NavigationRail(
+      body: Obx(
+        () => Row(
+          children: <Widget>[
+            if (_responsiveController.platform.value == Platform.desktop)
+              NavigationRail(
                 selectedIndex: _homePageController.selectedIndex.value,
                 onDestinationSelected: (int index) {
                   _homePageController.selectedIndex.value = index;
@@ -37,17 +40,43 @@ class HomePage extends StatelessWidget {
                     label: Text('Third'),
                   ),
                 ],
-              )),
-          const VerticalDivider(thickness: 1, width: 1),
-          // This is the main content.
-          Expanded(
-            child: Center(
-              child: Obx(
-                () => Text("${_responsiveController.platform.value}"),
+              ),
+            const VerticalDivider(thickness: 1, width: 1),
+            // This is the main content.
+            Expanded(
+              child: Center(
+                child: Obx(
+                  () => Text("${_responsiveController.platform.value}"),
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
+      ),
+
+      bottomNavigationBar: Obx(
+        () => _responsiveController.platform.value == Platform.desktop
+            ? const SizedBox(width: 0, height: 0,)
+            : BottomNavigationBar(
+                currentIndex: _homePageController.selectedIndex.value,
+                onTap: (int index) {
+                  _homePageController.selectedIndex.value = index;
+                },
+                items: <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                    icon: _homePageController.selectedIndex.value == 0 ? const Icon(Icons.favorite) : const Icon(Icons.favorite_border),
+                    label: 'First',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: _homePageController.selectedIndex.value == 1 ? const Icon(Icons.bookmark) : const Icon(Icons.bookmark_border),
+                    label: 'Second',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: _homePageController.selectedIndex.value == 2 ? const Icon(Icons.star) : const Icon(Icons.star_border),
+                    label: 'Third',
+                  ),
+                ],
+              ),
       ),
     );
   }
