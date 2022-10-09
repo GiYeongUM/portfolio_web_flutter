5c5a732d2d864ac8e7898075798089afdf04688c
GiYeong Um <66294595+GiYeongUM@users.noreply.github.com>
2022-10-09 12:15:49 +0900

    feat: career_page.dart, dev_page.dart, project_page.dart 추가

---
+++ lib/page/career_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CareerPage extends StatelessWidget {
+  const CareerPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/page/dev_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DevPage extends StatelessWidget {
+  const DevPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
