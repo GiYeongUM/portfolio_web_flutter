0e16d015c2d2251e10490e75d6d33dd4b4eab73e
GiYeong Um <eomky2005@gmail.com>
2023-12-15 17:37:12 +0900

    feat | 1.2.0 | strength uiux 완료
    Signed-off-by: GiYeong Um <eomky2005@gmail.com>

--- lib/features/strength/ui/strength_page.dart
+++ lib/features/strength/ui/strength_page.dart
@@ -15,14 +15,14 @@ class StrengthPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: context.colorTheme.backgroundColor,
-      body: Padding(
-        padding: context.isDesktop ? const EdgeInsets.symmetric(horizontal: 64) : const EdgeInsets.symmetric(horizontal: 24),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 24),
-            Column(
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 40),
+          Container(
+            margin: context.isDesktop ? const EdgeInsets.symmetric(horizontal: 64) : const EdgeInsets.symmetric(horizontal: 24),
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Hero(tag: '${strengthWidgets(context)[index].title}', child: Text("${strengthWidgets(context)[index].title}", style: context.textTheme.krSubtitle1)),
@@ -33,37 +33,38 @@ class StrengthPage extends StatelessWidget {
                     .moveX(begin: 24, end: 0, duration: 500.ms, delay: 500.ms, curve: Curves.easeOut),
               ],
             ),
-            const SizedBox(height: 24),
-            Expanded(child: strengthWidget(index)),
-            const SizedBox(height: 24),
-            Align(
-              alignment: Alignment.centerRight,
-              child: InkWell(
-                onTap: () => context.pop(),
-                child: Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-                    decoration: BoxDecoration(
-                      color: context.colorTheme.primaryColor,
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        const Icon(
-                          Icons.arrow_circle_left_rounded,
-                          color: white,
-                          size: 16,
-                        ),
-                        const SizedBox(width: 8),
-                        Text(context.localization.back, style: context.textTheme.krButton1.copyWith(color: white)),
-                      ],
-                    )),
-              ),
-            ).animate().fadeIn(duration: 500.ms, delay: 2000.ms),
-            const SizedBox(height: 24),
-          ],
-        ),
+          ),
+          const SizedBox(height: 40),
+          Expanded(child: strengthWidget(index)),
+          const SizedBox(height: 24),
+          Align(
+            alignment: Alignment.centerRight,
+            child: InkWell(
+              onTap: () => context.pop(),
+              child: Container(
+                  margin: context.isDesktop ? const EdgeInsets.symmetric(horizontal: 64) : const EdgeInsets.symmetric(horizontal: 24),
+                  padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                  decoration: BoxDecoration(
+                    color: context.colorTheme.primaryColor,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Icon(
+                        Icons.arrow_circle_left_rounded,
+                        color: white,
+                        size: 16,
+                      ),
+                      const SizedBox(width: 8),
+                      Text(context.localization.back, style: context.textTheme.krButton1.copyWith(color: white)),
+                    ],
+                  )),
+            ),
+          ).animate().fadeIn(duration: 500.ms, delay: 2000.ms),
+          const SizedBox(height: 24),
+        ],
       ),
     );
   }
--- lib/features/strength/widget/strength_uiux_widget.dart
+++ lib/features/strength/widget/strength_uiux_widget.dart
@@ -1,9 +1,9 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:video_player/video_player.dart';
 
 import '../../../core/core.dart';
-import '../../intro/widget/hover_change_widget.dart';
 
 class StrengthUiUxWidget extends StatefulWidget {
   const StrengthUiUxWidget({super.key});
@@ -13,54 +13,68 @@ class StrengthUiUxWidget extends StatefulWidget {
 }
 
 class _StrengthUiUxWidgetState extends State<StrengthUiUxWidget> {
-  late VideoPlayerController _firstVideoPlayerController;
-  late VideoPlayerController _secondVideoPlayerController;
+  late List<VideoPlayerController> _videoPlayerControllers;
+
+  final ScrollController scrollController = ScrollController();
 
   @override
   void initState() {
     super.initState();
-    _firstVideoPlayerController = VideoPlayerController.asset('assets/videos/ui_ux_1.mp4');
-    _secondVideoPlayerController = VideoPlayerController.asset('assets/videos/ui_ux_2.mp4');
-    _firstVideoPlayerController.initialize().then((_) {
-      _firstVideoPlayerController.setVolume(0);
-      _firstVideoPlayerController.play();
-      _firstVideoPlayerController.setLooping(true);
-    });
-    _secondVideoPlayerController.initialize().then((_) {
-      _secondVideoPlayerController.setVolume(0);
-      _secondVideoPlayerController.play();
-      _secondVideoPlayerController.setLooping(true);
-    });
+    _videoPlayerControllers = [
+      VideoPlayerController.asset('assets/videos/ui_ux_1.mp4'),
+      VideoPlayerController.asset('assets/videos/ui_ux_2.mp4'),
+      VideoPlayerController.asset('assets/videos/ui_ux_3.mp4'),
+      VideoPlayerController.asset('assets/videos/ui_ux_4.mp4'),
+      VideoPlayerController.asset('assets/videos/ui_ux_5.mp4'),
+    ];
+    for (var element in _videoPlayerControllers) {
+      element.initialize().then((_) {
+        element.setVolume(0);
+        element.play();
+        element.setLooping(true);
+      });
+    }
+  }
+
+  onPointerScroll(PointerScrollEvent pointerSignal) {
+    if (pointerSignal.scrollDelta.dy != 0 &&
+        scrollController.offset + pointerSignal.scrollDelta.dy >= -200 &&
+        scrollController.offset + pointerSignal.scrollDelta.dy <= scrollController.position.maxScrollExtent + 200) {
+      scrollController.jumpTo(scrollController.offset + pointerSignal.scrollDelta.dy);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: context.isDesktop ? MainAxisAlignment.start : MainAxisAlignment.spaceAround,
-      children: [
-        HoverChangeWidget(
-          type: HoverType.up,
-          onInitialAnimation: false,
-          animatedChild: Container(
-            constraints: BoxConstraints(maxHeight: MediaQuery.of(context).size.height / 1.5),
-            width: context.isDesktop ? null : MediaQuery.of(context).size.width / 2.5,
-            child: AspectRatio(
-              aspectRatio: 886 / 1920,
-              child: ClipRRect(borderRadius: BorderRadius.circular(8), child: VideoPlayer(_firstVideoPlayerController)),
-            ),
-          ),
-        ).animate().fadeIn(duration: 500.ms, delay: 1.seconds).moveX(begin: 24, end: 0, duration: 500.ms, delay: 1.seconds, curve: Curves.easeOut),
-        if (context.isDesktop) const SizedBox(width: 40),
-        HoverChangeWidget(
-          type: HoverType.up,
-          onInitialAnimation: false,
-          animatedChild: Container(
-              constraints: BoxConstraints(maxHeight: MediaQuery.of(context).size.height / 1.5),
-              width: context.isDesktop ? null : MediaQuery.of(context).size.width / 2.5,
-              child: AspectRatio(aspectRatio: 886 / 1920, child: ClipRRect(borderRadius: BorderRadius.circular(8), child: VideoPlayer(_secondVideoPlayerController)))),
-        ).animate().fadeIn(duration: 500.ms, delay: 1500.ms).moveX(begin: 24, end: 0, duration: 500.ms, delay: 1500.ms, curve: Curves.easeOut),
-      ],
+    return Listener(
+      onPointerSignal: (pointerSignal) {
+        if (pointerSignal is PointerScrollEvent) onPointerScroll(pointerSignal);
+      },
+      child: SingleChildScrollView(
+        controller: scrollController,
+        scrollDirection: Axis.horizontal,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: _videoPlayerControllers
+                  .asMap()
+                  .entries
+                  .map(
+                    (e) => Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 16),
+                      constraints: BoxConstraints(maxHeight: MediaQuery.of(context).size.height / 1.5),
+                      width: context.isDesktop ? null : MediaQuery.of(context).size.width / 2.5,
+                      child: AspectRatio(
+                        aspectRatio: 886 / 1920,
+                        child: ClipRRect(borderRadius: BorderRadius.circular(8), child: VideoPlayer(e.value)),
+                      ),
+                    ).animate().fadeIn(duration: 500.ms, delay: (500 + e.key * 500).ms).moveX(begin: 24, end: 0, duration: 500.ms, delay: (500 + e.key * 500).ms, curve: Curves.easeOut),
+                  )
+                  .toList()),
+        ),
+      ),
     );
   }
 }
